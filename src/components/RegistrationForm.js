8b1b5c822ecf4ae54ffd7ec261fe1e2174ba1e2a
edwin rivera <waehake@gmail.com>
2018-01-29 15:08:02 +0000

    automatically backed up by learn

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 
 class RegistrationForm extends Component {
   render() {
-    
+    return (
+      <div>
+        <form>
+          <input type="text" name="username">
+          <input type="password" name="password">
+        </form>
+      </div>
+      );
   }
 }
 
